f7696e81d7a7603f6f751691c16b994074ae8b74
Lih Chen <lihster@gmail.com>
2017-10-28 00:04:16 -0700

    Added swipeGestureBegan to README and PropTypes

--- components/SwipeListView.js
+++ components/SwipeListView.js
@@ -44,7 +44,6 @@ class SwipeListView extends Component {
 		if (this.props.swipeGestureBegan) {
 			this.props.swipeGestureBegan(id);
 		}
-
 	}
 
 	onRowOpen(secId, rowId, rowMap) {
@@ -210,6 +209,10 @@ SwipeListView.propTypes = {
 	 * One case is a SwipeListView with rows of different heights and an options to delete rows.
 	 */
 	recalculateHiddenLayout: PropTypes.bool,
+	/**
+	 * Called when a swipe row is animating swipe
+	 */
+	swipeGestureBegan: PropTypes.func,
 	/**
 	 * Called when a swipe row is animating open
 	 */
